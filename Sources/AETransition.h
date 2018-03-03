84be329eccd42ce526c8b0429f674de3c6ec1754
Marko Tadic <tadija@me.com>
2018-03-03 21:38:13 +0100

    Moved old sources into Backup directory and created new framework project

---
+++ Sources/AETransition.h
@@ -0,0 +1,19 @@
+//
+//  AETransition.h
+//  AETransition
+//
+//  Created by Marko Tadić on 3/3/18.
+//  Copyright © 2018 AE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AETransition.
+FOUNDATION_EXPORT double AETransitionVersionNumber;
+
+//! Project version string for AETransition.
+FOUNDATION_EXPORT const unsigned char AETransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AETransition/PublicHeader.h>
+
+
