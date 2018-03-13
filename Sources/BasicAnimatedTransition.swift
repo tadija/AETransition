995229dbfb5008ca692ad7d7f730d30ce84bc97d
Marko Tadic <tadija@me.com>
2018-03-13 22:26:21 +0100

    Created initial version of BasicAnimatedTransition (and a few test layers)

--- Sources/BasicAnimatedTransition.swift
+++ Sources/BasicAnimatedTransition.swift
@@ -6,6 +6,71 @@
 
 import UIKit
 
+public protocol AnimatedTransitionLayer {
+    var preparation: AnimatedTransition.ContextHandler? { get }
+    var animation: AnimatedTransition.ContextHandler? { get }
+}
+
+open class BasicAnimatedTransition: AnimatedTransition {
+
+    // MARK: Properties
+
+    open let layer: AnimatedTransitionLayer
+
+    open var completion: ContextHandler? = { (context) in
+        context.completeTransition(!context.transitionWasCancelled)
+    }
+
+    // MARK: Init
+
+    public init(layer: AnimatedTransitionLayer, duration: TimeInterval = 0.5) {
+        self.layer = layer
+        
+        super.init(duration: duration)
+
+        transitionAnimation = { [weak self] (context) in
+            self?.layer.preparation?(context)
+            UIView.animate(withDuration: duration, animations: {
+                self?.layer.animation?(context)
+            }, completion: { (finished) in
+                self?.completion?(context)
+            })
+        }
+    }
+
+}
+
+public struct FadeInLayer: AnimatedTransitionLayer {
+    public var preparation: AnimatedTransition.ContextHandler? = { (context) in
+        context.insertToViewAboveFromView()
+        context.toView?.alpha = 0
+    }
+    public var animation: AnimatedTransition.ContextHandler? = { (context) in
+        context.toView?.alpha = 1
+    }
+}
+
+public struct FadeOutLayer: AnimatedTransitionLayer {
+    public var preparation: AnimatedTransition.ContextHandler? = { (context) in
+        context.insertToViewBelowFromView()
+    }
+    public var animation: AnimatedTransition.ContextHandler? = { (context) in
+        context.fromView?.alpha = 0
+    }
+}
+
+open class LayeredFadeInTransition: BasicAnimatedTransition {
+    public init(duration: TimeInterval = 0.5) {
+        super.init(layer: FadeInLayer(), duration: duration)
+    }
+}
+
+open class LayeredFadeOutTransition: BasicAnimatedTransition {
+    public init(duration: TimeInterval = 0.5) {
+        super.init(layer: FadeOutLayer(), duration: duration)
+    }
+}
+
 open class FadeInTransition: AnimatedTransition {
     public init(duration: TimeInterval = 0.5) {
         super.init(duration: duration)
