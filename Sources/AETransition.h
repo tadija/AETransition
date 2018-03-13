053c86a58ead5784b5677921a73ef5647d5fc173
Marko Tadic <tadija@me.com>
2018-03-13 20:15:29 +0100

    Minor changes

--- Sources/AETransition.h
+++ Sources/AETransition.h
@@ -6,12 +6,5 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for AETransition.
 FOUNDATION_EXPORT double AETransitionVersionNumber;
-
-//! Project version string for AETransition.
 FOUNDATION_EXPORT const unsigned char AETransitionVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <AETransition/PublicHeader.h>
-
-
